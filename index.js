2d230f5228ad381fbcd6d5b6d16b932a6b45b5ea
Ariana <152944814+aam214@users.noreply.github.com>
2024-05-22 15:43:10 -0500

    Moved form element. Removed unnecessary divs and classes. Added confirm password input. Changed button html. Edited head due to width error. Edited alignment, margin and padding in the form. Added variables to JS to getElementById.

---
+++ index.js
@@ -0,0 +1,10 @@
+const form = document.getElementById('form');
+const nameField = document.getElementById('nameField');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+signIn.onClick = function() {
+  nameField.style.maxHeight = "0";
+
+}
