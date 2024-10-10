5fdc5b7d3d4414577bd5f4f3f0999216e5661b2e
Ariana <152944814+aam214@users.noreply.github.com>
2024-10-09 20:14:07 -0500

    Edited styling of buttons. Changed class name to .error for variable of errorDisplay. Removed sign-in button. Changed a element to a button. Renamed class for button. Added Heading to website.

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const setError = (element, message) => {
 
 const setSuccess = element => {
     const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.success');
+    const errorDisplay = inputControl.querySelector('.error');
     errorDisplay.innerText = '';
     inputControl.classList.add('success');
     inputControl.classList.remove('error');
